f1a08ce9ec5063c3a697ef7373d1f91df1c3f2c5
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-30 14:10:43 +0530

    Added Logger and Axios setup for frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "../stylesheets/application.scss"
 
+import "../stylesheets/application.scss";
+
+const { setAuthHeaders } = require("apis/axios");
+const { initializeLogger } = require("common/logger");
+
+initializeLogger();
+setAuthHeaders();
