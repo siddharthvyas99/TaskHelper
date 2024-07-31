e95f9858cd03dd10c72126b31dbb8ac6f9328083
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 15:47:49 +0530

    Added backend model validation for Title length

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
 
   validates :title,
     presence: true,
+    length: { maximum: MAX_TITLE_LENGTH },
     format: { with: VALID_TITLE_REGEX }
   validates :slug, uniqueness: true
   validate :slug_not_changed
