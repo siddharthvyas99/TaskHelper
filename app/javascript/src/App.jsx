4fde8a8503874f3d54f675382e2a622607c86add
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 15:45:56 +0530

    Added Tasks component to Tasks index file, added frontend routes for     Update and Show tasks

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import Dashboard from "components/Dashboard";
-import CreateTask from "components/Tasks/Create";
+import { CreateTask, ShowTask, EditTask } from "components/Tasks";
 
 const App = () => (
   <Router>
     <ToastContainer />
     <Switch>
+      <Route exact path="/tasks/:slug/edit" component={EditTask} />
+      <Route exact path="/tasks/:slug/show" component={ShowTask} />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
     </Switch>
