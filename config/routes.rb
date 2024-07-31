9fab46f7801ed35e12774469c9eb3d4300f407cb
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 15:14:39 +0530

    Added slug handling for Tasks API using params

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :tasks
+  resources :tasks, param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
