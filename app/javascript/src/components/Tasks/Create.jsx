ecc0b78d2fc000cded6bd438462b4fe0a356b24b
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 22:04:31 +0530

    Refactored Task Form to include assignee field, updated Create comp.     to handle user changes     - Refactored Tasks Create component to add user fetch details,       default user field, and user handling states

--- app/javascript/src/components/Tasks/Create.jsx
+++ app/javascript/src/components/Tasks/Create.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import tasksApi from "apis/tasks";
-import { Container, PageTitle } from "components/commons";
+import usersApi from "apis/users";
+import { Container, PageLoader, PageTitle } from "components/commons";
 
 import Form from "./Form";
 
 const Create = ({ history }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [userId, setUserId] = useState("");
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [pageLoading, setPageLoading] = useState(true);
 
   const handleSubmit = async event => {
     event.preventDefault();
     setLoading(true);
     try {
-      await tasksApi.create({ task: { title, description } });
+      await tasksApi.create({
+        task: { title, description, assigned_user_id: userId },
+      });
       setLoading(false);
       history.push("/dashboard");
     } catch (error) {
@@ -23,17 +29,42 @@ const Create = ({ history }) => {
     }
   };
 
+  const fetchUserDetails = async () => {
+    try {
+      const {
+        data: { users },
+      } = await usersApi.fetch();
+      setUsers(users);
+      setUserId(users[0]?.id);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setPageLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
+  if (pageLoading) {
+    return <PageLoader />;
+  }
+
   return (
     <Container>
       <div className="flex flex-col gap-y-8">
         <PageTitle title="Add new task" />
         <Form
+          assignedUser={users[0]}
           description={description}
           handleSubmit={handleSubmit}
           loading={loading}
           setDescription={setDescription}
           setTitle={setTitle}
+          setUserId={setUserId}
           title={title}
+          users={users}
         />
       </div>
     </Container>
--- app/javascript/src/components/Tasks/Form.jsx
+++ app/javascript/src/components/Tasks/Form.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { Input, Button } from "components/commons";
+import Select from "react-select";
+
+import { Button, Input } from "components/commons";
 
 const Form = ({
   type = "create",
@@ -8,29 +10,69 @@ const Form = ({
   description,
   setTitle,
   setDescription,
+  assignedUser,
+  users,
+  setUserId,
   loading,
   handleSubmit,
-}) => (
-  <form className="mb-4 w-full space-y-2" onSubmit={handleSubmit}>
-    <Input
-      label="Title"
-      placeholder="Todo Title (Max 125 Characters Allowed)"
-      value={title}
-      onChange={e => setTitle(e.target.value.slice(0, 125))}
-    />
-    <Input
-      label="Description"
-      placeholder="Todo Description"
-      type="textaread"
-      value={description}
-      onChange={e => setDescription(e.target.value)}
-    />
-    <Button
-      buttonText={type === "create" ? "Create Task" : "Update Task"}
-      loading={loading}
-      type="submit"
-    />
-  </form>
-);
+}) => {
+  // const history = useHistory();
+  const userOptions = users.map(user => ({
+    value: user.id,
+    label: user.name,
+  }));
+  const defaultOption = { value: assignedUser?.id, label: assignedUser?.name };
+
+  // const initialValues = useRef({
+  //   title,
+  //   userId: assignedUser?.id,
+  // });
+
+  // const isNotDirty = equals(initialValues.current, {
+  //   title,
+  //   userId: assignedUser?.id,
+  // });
+
+  return (
+    <form className="mb-4 w-full space-y-2" onSubmit={handleSubmit}>
+      <div className="mx-auto mb-4 w-full overflow-hidden rounded-lg border border-gray-200 bg-white text-gray-800 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-2xl">
+        <div className="flex flex-col gap-4 p-6">
+          <Input
+            label="Title"
+            placeholder="Todo Title (Max 125 Characters Allowed)"
+            value={title}
+            onChange={e => setTitle(e.target.value.slice(0, 125))}
+          />
+          <Input
+            label="Description"
+            placeholder="Todo Description"
+            type="textaread"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <div className="flex flex-col">
+            <p className="text-sm font-medium leading-none text-gray-800">
+              Assigned To
+            </p>
+            <div className="mt-1 w-full">
+              <Select
+                isSearchable
+                defaultValue={defaultOption}
+                menuPosition="fixed"
+                options={userOptions}
+                onChange={e => setUserId(e.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Button
+        buttonText={type === "create" ? "Create Task" : "Update Task"}
+        loading={loading}
+        type="submit"
+      />
+    </form>
+  );
+};
 
 export default Form;
