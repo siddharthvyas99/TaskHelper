7e978d1ccdf072d015e9bb513b676b32c6bfaea1
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 14:43:34 +0530

    Added support for Toastr notification using react-toastify     - Added react-toastify dependency with styling setup     - Added common Toastr component, updated imports     - Added common constant and util functions     - Added aliasing for utils and constant direcs. in resolve     - Wired Toastr component

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Dashboard from "components/Dashboard";
+import CreateTask from "components/Tasks/Create";
 
 const App = () => (
   <Router>
+    <ToastContainer />
     <Switch>
-      <Route exact path="/" render={() => <div>Home</div>} />
+      <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
     </Switch>
   </Router>
