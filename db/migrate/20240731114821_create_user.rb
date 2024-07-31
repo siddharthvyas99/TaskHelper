957aae05ca2728cbbda5267942a5200db8ea1450
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-31 18:26:43 +0530

    Added migration and model for User

---
+++ db/migrate/20240731114821_create_user.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateUser < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_061446) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_31_114821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_061446) do
     t.datetime "updated_at", null: false
     t.string "slug", null: false
     t.index ["slug"], name: "index_tasks_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
